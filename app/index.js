e63dffc233f6722ea6813a88442dff60e2e76a5a
Keevin Mitchell <keevin.mitchell@gmail.com>
2020-07-12 10:08:54 -0400

    Secont commit not animation

---
+++ app/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Image, Dimensions } from 'react-native';
+const {width, height} = Dimensions.get('window');
+
+ class CovidCare extends Component {
+  
+
+  render() {
+    return (
+      <View style={{flex: 1, backgroundColor: 'white', justifyContent: 'flex-end'}}>
+          <View style={{...StyleSheet.absoluteFill}}>
+
+              <Image
+                source={require('../assets/Killing_Viruses_-_Finding_the_Cure_-_Coronavirus.jpg')}
+                style={{ flex: 1, height: null, width:null}}
+                />
+
+          </View>
+
+          <View style={{height:height / 3, justifyContent: 'center'}}>
+
+              <View style={styles.button}>
+                  <Text style={{ fontSize: 20, fontWeight: 'bold'}}>SIGN IN</Text>
+              </View>
+
+              <View style={{...styles.button, backgroundColor: '#2E71Dc'}}>
+                  <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'white'}}>SIGN IN WITH FACEBOOK</Text>
+              </View>
+
+          </View>
+        
+      </View>
+    );
+  }
+}
+
+export default CovidCare;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    button: {
+        backgroundColor: 'white',
+        height: 70,
+        marginHorizontal: 20,
+        borderRadius: 35,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginVertical: 5
+    }
+})
